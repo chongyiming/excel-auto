949935e3734667203c79ecbd98aba7c90fce8f16
chong yi ming <107567026+chongyiming@users.noreply.github.com>
2025-09-25 09:57:27 +0800

    test2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import streamlit as st
+import streamlit as st
 # import pandas as pd
 # from io import StringIO
 # import ast
@@ -85,3 +85,4 @@
 
 #     except Exception as e:
 #         st.error(f"Error parsing table: {e}")
+st.image("cannot-do.png")
