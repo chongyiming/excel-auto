532c31f7c3848c9fad4dc0a2b0801ce7cfe29fc7
chong yi ming <107567026+chongyiming@users.noreply.github.com>
2025-09-25 09:50:26 +0800

    test

--- pages/requestTimeout.py
+++ pages/requestTimeout.py
@@ -1,40 +1,10 @@
-# import re
-
-# completed_ids = set()
-# all_matches = []
-
-# # Step 1: 读取日志文件
-# with open("logfile.txt", "r", encoding="utf-8") as file:
-#     lines = file.readlines()
-#     print(lines[0])
-
-# # Step 2: 找出所有 completed 的 orderid
-# for line in lines:
-#     if re.search(r"\bcompleted\b", line, re.IGNORECASE):
-#         found = re.findall(r"#\d+", line)
-#         completed_ids.update(found)
-
-# # Step 3: 找出所有非 completed 的 orderid
-# for line in lines:
-#     found = re.findall(r"#\d+", line)
-#     for order_id in found:
-#         if order_id not in completed_ids:
-#             all_matches.append(order_id)
-
-# # Step 4: 去重并按数字排序
-# unique_matches = sorted(set(all_matches), key=lambda x: int(x[1:]))
-
-# # Step 5: 写入文件
-# with open("unique_hash_numbers.txt", "w", encoding="utf-8") as output:
-#     output.write(",".join(unique_matches))
-
-# print("✅ Done! Saved to 'unique_hash_numbers.txt'")
-
 import streamlit as st
 import pandas as pd
 from io import StringIO
 import ast
 import re
+
+
 
 completed_ids = set()
 all_matches = []
@@ -57,7 +27,7 @@ if log_text:
                     all_matches.append(order_id)
 
         unique_matches = sorted(set(all_matches), key=lambda x: int(x[1:]))
-
+        
         st.write(", ".join(unique_matches))
 
 
