c41321c12c2731b6c69e278b0e17fd3e0d31a476
chong yi ming <107567026+chongyiming@users.noreply.github.com>
2025-09-27 23:59:31 +0800

    test

--- pages/request_timeout.py
+++ pages/request_timeout.py
@@ -1,60 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# from io import StringIO
-# from collections import defaultdict
-# from datetime import datetime, timedelta
-
-
-# rt_pasted_text = st.text_area("Request Timeout (Get all accounts)")
-
-# result_dict = defaultdict(list)
-# if rt_pasted_text:
-#     try:
-#         rt_df = pd.read_csv(StringIO(rt_pasted_text), sep="\t", header=None)
-
-#         # Loop through each row
-#         for i in range(len(rt_df)):
-#             key = str(rt_df.iloc[i, 0])
-#             full_string = str(rt_df.iloc[i, 3])
-#             value = full_string.split(':')[0]  # Get part before the colon
-#             result_dict[key].append((str(rt_df.iloc[i, 1]), value[1:-1]))
-
-#         for i in result_dict:
-#             st.write(i, ":")
-#             output_string = ""
-#             for _, value in result_dict[i]:
-#                 output_string += "'"+value+"'" + "|"
-#             st.write(output_string[0:-1])
-#         print(result_dict)
-#         st.write("2) Paste journal:")
-
-#         columns = st.columns(len(result_dict))
-#         text_inputs = {}
-
-#         for i, (key, _) in enumerate(result_dict.items()):
-#             with columns[i]:
-#                 text_inputs[key] = st.text_area(f"Journal for {key}")
-
-#         df_dict=defaultdict(list)
-#         if all(len(text_inputs[key].strip()) > 0 for key in result_dict):
-#             for key in text_inputs:
-#                 for i, value_list in result_dict.items():
-#                     if i == key:
-#                         journal_df = pd.read_csv(StringIO(text_inputs[key]), sep="\t", header=None)
-#                         for i in value_list:
-#                             matching_rows = journal_df[journal_df[2].str.match(fr"'{i[1]}':")]
-#                             st.write(matching_rows)
-
-
-
-
-
-#         else:
-#             st.warning("Please fill in all input fields before proceeding.")
-#     except:
-#         pass
-
-
 import re
 
 import streamlit as st
@@ -171,33 +114,39 @@ if rt_pasted_text:
                     else:
                         st.write(f"No logs for `{login_id}`")
 
-
             st.markdown("""<br>""", unsafe_allow_html=True)
 
                 
 
-            for key in all_matches:
+            st.markdown("""<br>""", unsafe_allow_html=True)
+            st.subheader("🧾 Final Summary: All Orders Per Account (including fallback)")
 
+            for key in result_dict:
                 all_orders_flat = []
+                login_ids_checked = set()
 
-                for login_id in all_matches[key]:
-                    order_ids = all_matches[key][login_id]
-                    unique_matches = sorted(set(order_ids), key=lambda x: int(x[1:]))
+                for _, login_id in result_dict[key]:  # Use all login IDs, not just those in all_matches
+                    login_ids_checked.add(login_id)
+                    orders = all_matches[key].get(login_id, [])
 
-                    all_orders_flat.extend(unique_matches)
-
-                        
-                st.markdown("""<br>""", unsafe_allow_html=True)
-
-                
+                    if orders:
+                        unique_orders = sorted(set(orders), key=lambda x: int(x[1:]))
+                        all_orders_flat.extend(unique_orders)
+                    else:
+                        # Fallback: use latest completed_id for this login_id
+                        completed_list = completed_ids[key].get(login_id, [])
+                        if completed_list:
+                            latest_completed = sorted(completed_list, key=lambda x: int(x[1:]))[-1]
+                            all_orders_flat.append(latest_completed)
 
                 summary_matches = sorted(set(all_orders_flat), key=lambda x: int(x[1:]))
+
+                st.markdown(f"### 🔑 Account: `{key}`")
                 if summary_matches:
-                    st.subheader(f"All orders for `{key}`:")
-                    st.write(",".join(summary_matches))
+                    st.write(", ".join(summary_matches))
                 else:
-                    st.subheader(f"All orders for `{key}`:")
-                    st.write("No incomplete orders found for this account.")
+                    st.write("No orders found (no incomplete or completed orders).")
+
 
 
 
