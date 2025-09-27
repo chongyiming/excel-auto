7e7a1e12ced1483aefbc12ed935e3a82fa6ff230
chong yi ming <107567026+chongyiming@users.noreply.github.com>
2025-09-28 00:02:23 +0800

    test

--- pages/get_order_from_journal.py
+++ pages/get_order_from_journal.py
@@ -1,42 +1,4 @@
-import streamlit as st
-import pandas as pd
-from io import StringIO
-import ast
-import re
-from collections import defaultdict
-
-
-rt_pasted_text = st.text_area("1) Paste Request Timeout (Get all accounts for each server)", height=200)
-
-result_dict = defaultdict(list)
-
-if rt_pasted_text:
-    try:
-        rt_df = pd.read_csv(StringIO(rt_pasted_text), sep="\t", header=None)
-
-        # Build result_dict
-        for i in range(len(rt_df)):
-            key = str(rt_df.iloc[i, 0])  # e.g. account
-            full_string = str(rt_df.iloc[i, 3])
-            value = full_string.split(':')[0]  # Get part before the colon
-            result_dict[key].append((str(rt_df.iloc[i, 1]), value[1:-1]))  # (timestamp, order_id)
-
-        # Display extracted order IDs
-        for account in result_dict:
-            st.write(account, ":")
-            order_ids = "|".join("'" + val + "'" for _, val in result_dict[account])
-            st.write(order_ids)
-    
-    except Exception as e:
-        st.error(f"Something went wrong: {e}")
-completed_ids = set()
-all_matches = []
-
-
-st.markdown("""
-<br>
-""", unsafe_allow_html=True)
-log_text = st.text_area("Paste log file here **(NOW ONLY SUPPORT INDIVIDUAL LOG)**", height=200, key="log_text")
+log_text = st.text_area("Paste log file here **(ONLY INDIVIDUAL LOG)**", height=200, key="log_text")
 if log_text:
     try:
         log_text_df = pd.read_csv(StringIO(log_text), sep="\t", header=None)
