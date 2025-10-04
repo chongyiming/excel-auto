27e9d02e9c1f85fe5f5062923d8db4752c492dc9
chong yi ming <107567026+chongyiming@users.noreply.github.com>
2025-10-04 15:33:13 +0800

    copy button

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+from st_copy import copy_button
 
 data = [
     ['Chloe Ngu'],
@@ -11,3 +11,23 @@ df = pd.DataFrame(data, columns=['Name'])
 
 
 st.dataframe(df)
+
+
+
+
+st.title("Copy Button Example")
+
+# Example text
+text_to_copy = "Hello, this is some text you can copy!"
+
+# Show the text
+st.code(text_to_copy, language="text")
+
+# # Add a copy button
+# copy_button(
+#     text_to_copy,
+#     icon='material_symbols',          # optional: Streamlit-style icon
+#     tooltip='Click to copy text',     # optional: hover tooltip
+#     copied_label='Copied!',           # optional: text shown after click
+#     key='copy_text_to_clipboard'      # required: must be unique per button
+# )
--- unused_groups.py
+++ unused_groups.py
@@ -2,25 +2,20 @@ import pandas as pd
 import streamlit as st
 import pandas as pd
 from io import StringIO
+from itables.streamlit import interactive_table
 
-# Text input from user
+
 sheet1 = st.text_area("Account", height=200, key="Account")
 sheet2 = st.text_area("Group", height=200, key="Group")
 
 sheet1_df = pd.read_csv(StringIO(sheet1), sep="\t", header=None)
-# st.write(sheet1_df)
 
-# Extract unique values from column 2 (index 2)
 groups_in_sheet1 = sheet1_df.iloc[:, 2].dropna().unique()
-# st.write("Groups in Sheet1:", groups_in_sheet1)
 
 
 sheet2_df = pd.read_csv(StringIO(sheet2), sep="\t", header=None)
-# st.write(sheet2_df)
 
-# Extract unique values from column 0 (index 0)
 groups_in_sheet2 = sheet2_df.iloc[:, 0].dropna().unique()
-# st.write("Groups in Sheet2:", groups_in_sheet2)
 
 exclude_list = ["datacenter", "coverage", "demoforex", "preliminary", "manager", "LIVE_instiview"]
 groups_in_sheet2 = [
@@ -30,7 +25,11 @@ groups_in_sheet2 = [
 
 unused_groups = [g for g in groups_in_sheet2 if g not in groups_in_sheet1]
 st.dataframe(unused_groups)
-# output_file = "unused_groups.xlsx"
-# pd.DataFrame(unused_groups, columns=["Unused Groups"]).to_excel(output_file, index=False)
 
-# print("Done", output_file)
+# # Convert the list into a DataFrame
+# unused_groups_df = pd.DataFrame(unused_groups, columns=["Unused Groups"])
+
+# # Show interactive table
+# interactive_table(unused_groups_df,
+#                   caption='testing',
+#                   buttons=['copyHtml5', 'colvis'])
